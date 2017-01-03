30ff8fd1375bd8da0561422f19fcd8bab087a4c4
Vijayabalan <vijayabalan@octoze.com>
2017-01-03 19:09:33 +0530

    shdb
    hsa

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-'use strict';
-
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
-};
-
-exports.grpBy = function(data, key){
-	var obj = {};
-	for (var i = data.length - 1; i >= 0; i--) {
-		obj[key] = data[i]
+module.exports = {
+	grpBy:function(data, key){
+		var obj = {};
+		for (var i = data.length - 1; i >= 0; i--) {
+			obj[data[i][key]] = data[i]
+		}
+		return obj
 	}
 }
